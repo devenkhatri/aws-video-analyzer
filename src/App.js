b2bae19061ee07425c6521962d5543aa18e8c6c3
Deven Goratela <deven.goratela@tcs.com>
2024-02-22 13:54:28 +0530

    base UI done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import styled from "styled-components";
-import DataTable from "react-data-table-component";
+import DataTable from "react-data-table-component"; //https://react-data-table-component.netlify.app/
 
 import { rekognitionClient } from "./libs/rekognitionClient.js";
 import { s3Client } from "./libs/s3Client.js";
 import { sesClient } from "./libs/sesClient.js";
 import { SendEmailCommand } from "@aws-sdk/client-ses";
-import { DeleteObjectCommand, ListObjectsCommand } from "@aws-sdk/client-s3";
+import {
+  DeleteObjectCommand,
+  ListObjectsCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
 import {
   StartFaceDetectionCommand,
   GetFaceDetectionCommand,
@@ -70,8 +74,9 @@ const tableCustomStyles = {
 };
 
 const App = () => {
-
-  const [newVideo, setNewVideo] = useState("No Video Selected");
+  const [newVideo, setNewVideo] = useState();
+  const [tableData, setTableData] = useState([]);
+  const [pending, setPending] = useState(false);
 
   const columns = [
     {
@@ -92,19 +97,6 @@ const App = () => {
     },
   ];
 
-  const data = [
-    {
-      id: 1,
-      title: "Beetlejuice",
-      year: "1988",
-    },
-    {
-      id: 2,
-      title: "Ghostbusters",
-      year: "1984",
-    },
-  ];
-
   // Upload the video.
   const uploadVideo = async () => {
     try {
@@ -115,35 +107,126 @@ const App = () => {
       console.log("Object in bucket: ", listObjects);
       console.log("listObjects.Contents ", listObjects.Contents);
 
-      const noOfObjects = listObjects.Contents;
-      // If the Amazon S3 bucket is not empty, delete the existing content.
-      if (noOfObjects != null) {
-        for (let i = 0; i < noOfObjects.length; i++) {
-          const data = await s3Client.send(
-            new DeleteObjectCommand({
-              Bucket: BUCKET,
-              Key: listObjects.Contents[i].Key,
-            })
-          );
-        }
-      }
-      console.log("Success - bucket empty.");
+      // const noOfObjects = listObjects.Contents;
+      // // If the Amazon S3 bucket is not empty, delete the existing content.
+      // if (noOfObjects != null) {
+      //   for (let i = 0; i < noOfObjects.length; i++) {
+      //     const data = await s3Client.send(
+      //       new DeleteObjectCommand({
+      //         Bucket: BUCKET,
+      //         Key: listObjects.Contents[i].Key,
+      //       })
+      //     );
+      //   }
+      // }
+      // console.log("Success - bucket empty.");
 
       // Create the parameters for uploading the video.
       // const videoName = document.getElementById("videoname").innerHTML + ".mp4";
       // const files = document.getElementById("videoupload").files;
       // const file = files[0];
-      // const uploadParams = {
-      //   Bucket: BUCKET,
-      //   Body: file,
-      // };
-      // uploadParams.Key = path.basename(file.name);
-      // const data = await s3Client.send(new PutObjectCommand(uploadParams));
-      // console.log("Success - video uploaded");
+      console.log("***** File", newVideo);
+      const uploadParams = {
+        Bucket: BUCKET,
+        Body: newVideo,
+      };
+      uploadParams.Key = newVideo.name;
+      const data = await s3Client.send(new PutObjectCommand(uploadParams));
+      console.log("Success - video uploaded", data);
+    } catch (err) {
+      console.log("Error while uploading video to S3", err);
+    }
+  };
+
+  //Get all videos from S3
+const getAllVideos = async () => {
+  try {
+    const listVideoParams = {
+      Bucket: BUCKET
+    };
+    setPending(true);
+    const data = await s3Client.send(new ListObjectsCommand(listVideoParams));
+    console.log("Success - available videos", data);
+    const formatedData = data.Contents.map(((item) => {
+      return  {
+        id: item.ETag,
+        name: item.Key,
+        owner: item.Owner.DisplayName,
+        date: item.LastModified.toISOString(),
+        size: (parseInt(item.Size) / 1024 / 1024).toFixed(2) + " MB"
+      }
+    }))
+
+    setTableData(formatedData);
+    setPending(false);
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+const processVideo = async () => {
+  try {
+    // Create the parameters required to start face detection.
+    const videoName = document.getElementById("videoname").innerHTML;
+    const startDetectParams = {
+      Video: {
+        S3Object: {
+          Bucket: BUCKET,
+          Name: videoName
+        },
+      },
+    };
+    // Start the Amazon Rekognition face detection process.
+    const data = await rekognitionClient.send(
+      new StartFaceDetectionCommand(startDetectParams)
+    );
+    console.log("Success, face detection started. ", data);
+    const faceDetectParams = {
+      JobId: data.JobId,
+    };
+    try {
+      var finished = false;
+      var facesArray = [];
+      // Detect the faces.
+      while (!finished) {
+        var results = await rekognitionClient.send(
+          new GetFaceDetectionCommand(faceDetectParams)
+        );
+        // Wait until the job succeeds.
+        if (results.JobStatus == "SUCCEEDED") {
+          finished = true;
+        }
+      }
+      finished = false;
+      // Parse results into CVS format.
+      const noOfFaces = results.Faces.length;
+      var i;
+      for (i = 0; i < results.Faces.length; i++) {
+        var boundingbox = JSON.stringify(results.Faces[i].Face.BoundingBox);
+        var confidence = JSON.stringify(results.Faces[i].Face.Confidence);
+        var pose = JSON.stringify(results.Faces[i].Face.Pose);
+        var quality = JSON.stringify(results.Faces[i].Face.Quality);
+        var arrayfirst = [];
+        var arraysecond = [];
+        var arraythird = [];
+        var arrayforth = [];
+        arrayfirst.push(boundingbox);
+        arraysecond.push(confidence);
+        arraythird.push(pose);
+        arrayforth.push(quality);
+        arrayfirst.push(arraysecond);
+        arrayfirst.push(arraythird);
+        arrayfirst.push(arrayforth);
+        facesArray.push(arrayfirst);
+      }
+      console.log("Faces Detection Output: ",facesArray);
     } catch (err) {
       console.log("Error", err);
     }
-  };
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
 
   return (
     <Container>
@@ -152,12 +235,15 @@ const App = () => {
         <p>Upload a video to an Amazon S3 bucket that will be analyzed!</p>
         {/* <input type="file" name="file" value={newVideo} onChange={e => setNewVideo(e.target.files && e.target.files[0].name)} /> */}
         {/* <input type="file" name="file" value={newVideo} onChange={e => console.log(e.target.files && e.target.files[0])} /> */}
-        <input type="file" accept='video/*' 
-        onChange={({ target: {files}}) => {
-          files[0] && setNewVideo(files[0].name)
-        }}
-         />
-        <br />Selected Video = {newVideo}
+        <input
+          type="file"
+          accept="video/*"
+          onChange={({ target: { files } }) => {
+            files[0] && setNewVideo(files[0]);
+          }}
+        />
+        <br />
+        Selected Video = {(newVideo && newVideo.name) || "No Video Selected"}
         <br />
         <Button id="addvideo" onClick={uploadVideo}>
           Add video
@@ -168,13 +254,16 @@ const App = () => {
           Choose the following button to get information about the video to
           analyze.
         </p>
-        <Button onClick="getVideo()">Show Video</Button>
+        <Button onClick={getAllVideos}>Show Video</Button>
         <DataTable
+          title="List of Files"
           columns={columns}
-          data={data}
+          data={tableData}
           customStyles={tableCustomStyles}
+          progressPending={pending}
           highlightOnHover
           pointerOnHover
+          selectableRows
         />
       </Section>
       <Section>
@@ -186,7 +275,7 @@ const App = () => {
           <p>
             Click the following button to analyze the video and obtain a report
           </p>
-          <Button id="button" onClick="ProcessImages()">
+          <Button id="button" onClick={processVideo}>
             Analyze Video
           </Button>
         </div>
